9853c8f71a435a2e45c3fcf55c249dfc4507d3b3
stdSic <stdsic9900@naver.com>
2024-05-15 19:11:43 +0900

    ToyProject: Minesweeper

---
+++ Minesweeper/Minesweeper.cpp
@@ -0,0 +1,80 @@
+#define UNICODE
+#define _WIN32_WINNT 0x0A00
+#include "Control.h"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#define CLASS_NAME TEXT("Minesweeper")
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENRTY WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow){
+	WNDCLASSEX wcex = {
+		sizeof(wcex),
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,0,
+		hInst,
+		NULL, LoadCursor(NULL, IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW+1),
+		NULL,
+		CLASS_NAME,
+		NULL
+	};
+
+	RegisterClassEx(&wcex);
+
+	HWND hWnd = CreateWindowEx(
+				WS_EX_CLIENTEDGE,
+				CLASS_NAME,
+				CLASS_NAME,
+				WS_OVERLAPPEDWINDOW,
+				CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+				NULL,
+				(HMENU)NULL,
+				hInst,
+				NULL
+			);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	while(GetMessage(&msg, NULL, 0,0)){
+		TranslateMessage(&msg);
+		DipsatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam){
+	switch(iMessage){
+		case WM_CREATE:
+			return 0;
+
+		case WM_RBUTTONDOWN:
+			return 0;
+
+		case WM_LBUTTONDOWN:
+			return 0;
+
+		case WM_MOUSEMOVE:
+			return 0;
+
+		case WM_TIMER:
+			return 0;
+
+		case WM_PAINT:
+			{
+				PAINTSTRUCT ps;
+				HDC hdc = BeginPaint(hWnd, &ps);
+				
+				EndPaint(hWnd, &ps);
+			}
+			return 0;
+
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+	}
+
+	return (DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
